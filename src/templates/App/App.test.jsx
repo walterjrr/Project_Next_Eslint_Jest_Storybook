639699d87563aa1d8c55fa71b583e176128b93ed
walterjrr <jrwalter731@gmail.com>
2022-07-07 14:23:51 -0300

    configurando o jest-styled-components

--- src/templates/App/App.test.jsx
+++ src/templates/App/App.test.jsx
@@ -1,8 +1,15 @@
-import { render, screen } from '@testing-library/react';
+/* eslint-disable no-unused-vars */
+import { screen } from '@testing-library/react';
+import { renderTheme } from '../../styles/render-theme';
+import { theme } from '../../styles/theme';
 import Home from './Index';
 
 test('renders learn react link', () => {
-  render(<Home />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+  const { debug } = renderTheme(<Home />);
+  const HeadingContainer = screen.getByRole('heading', { name: 'Hello' }).parentElement;
+
+  expect(HeadingContainer).toHaveStyle({
+    background: theme.colors.mainBg,
+  });
+  expect(HeadingContainer).toMatchSnapshot();
 });
