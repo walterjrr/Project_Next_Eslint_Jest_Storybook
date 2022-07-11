f48736dcbff26bee3a9d3ca3196db37e208d0859
walterjrr <jrwalter731@gmail.com>
2022-07-11 11:39:14 -0300

    configurando as fonts do projeto

--- src/styles/theme.js
+++ src/styles/theme.js
@@ -4,6 +4,11 @@ export const theme = {
     secondaryColor: '#dc143c',
     white: '#ffffff',
   },
-  fonts: {},
+  fonts: {
+    family: {
+      default: "'Opens Sans', sans-serif",
+      secondary: "'Montserrat', sans-serif",
+    },
+  },
   spacings: {},
 };
