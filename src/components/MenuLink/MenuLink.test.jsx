4bdbdeb13296a8c129cc821f872f59ceb016a09f
walterjrr <jrwalter731@gmail.com>
2022-07-18 18:27:53 -0300

    testando o MenuLink

--- src/components/MenuLink/MenuLink.test.jsx
+++ src/components/MenuLink/MenuLink.test.jsx
@@ -3,8 +3,26 @@ import { MenuLink } from '.';
 import { screen } from '@testing-library/react';
 
 describe('<MenuLink />', () => {
-  it('should render', () => {
-    renderTheme(<MenuLink>Children</MenuLink>);
-    expect(screen.getByRole('heading')).toBeInTheDocument();
+  it('should render a link', () => {
+    renderTheme(<MenuLink link="http://localhost">Children</MenuLink>);
+    expect(screen.getByRole('link', { name: 'Children' })).toHaveAttribute('target', '_self');
+  });
+
+  it('should render open in a new tab', () => {
+    renderTheme(
+      <MenuLink link="http://localhost" newTab={true}>
+        Children
+      </MenuLink>,
+    );
+    expect(screen.getByRole('link', { name: 'Children' })).toHaveAttribute('target', '_blank');
+  });
+
+  it('should render open in a new tab', () => {
+    renderTheme(
+      <MenuLink link="http://localhost" newTab={false}>
+        Children
+      </MenuLink>,
+    );
+    expect(screen.getByRole('link', { name: 'Children' })).toMatchSnapshot();
   });
 });
